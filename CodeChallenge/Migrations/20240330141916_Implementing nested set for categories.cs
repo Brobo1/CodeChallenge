cbb6dfab3d75f374bffa36702b72fce7a2339b78
brobo1 <armanahz95@gmail.com>
2024-03-30 17:49:05 +0100

    Implement nested set model for categories
    This commit adds new migration files for applying the nested set model to categories. It includes the addition of 'Lft' and 'Rgt' columns to the 'Categories' table as well as the necessary navigation properties to build a tree-like structure. Empty migration files that were committed without changes have also been added.

---
+++ CodeChallenge/Migrations/20240330141916_Implementing nested set for categories.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeChallenge.Migrations
+{
+    /// <inheritdoc />
+    public partial class Implementingnestedsetforcategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Lft",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rgt",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lft",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Rgt",
+                table: "Categories");
+        }
+    }
+}
